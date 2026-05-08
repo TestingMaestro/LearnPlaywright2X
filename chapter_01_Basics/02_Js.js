1d4ee678febb9d53972ba41bf9dd247b44e3df1c
Yashodhar Karki <yashodhar.karkii@gmail.com>
2026-05-08 10:01:46 +0530

    Initial commit: Playwright basics setup files

---
+++ chapter_01_Basics/02_Js.js
@@ -0,0 +1,15 @@
+// Everything is matched and divided into tokens/chunks 
+// below code is converted to byte code by Iterpreter directly [Line by Line Execution]
+let x = 10;
+console.log(x);
+
+// Complex Code/Code that runs repeatedly or frequently/Hot code /frequently running Byte code
+
+for (let i = 0; i < 10000000; i++) {
+    console.log(i);
+    print();
+}
+
+function print() {
+    console.log("Hello");
+}
