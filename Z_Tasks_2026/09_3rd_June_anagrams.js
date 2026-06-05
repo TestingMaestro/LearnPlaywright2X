c256cb0ceb2b471829e12af9127a4ad888643808
Yashodhar Karki <yashodhar.karkii@gmail.com>
2026-06-05 06:34:34 +0530

    refactor: update 09_3rd_June_anagrams.js with case-insensitive sorting logic

--- Z_Tasks_2026/09_3rd_June_anagrams.js
+++ Z_Tasks_2026/09_3rd_June_anagrams.js
@@ -1,11 +1,14 @@
-let s = "listen"
-let t = "silent"
+let s = "tea"
+let t = "eat"
 
 let len1 = s.length
 let len2 = t.length
 
-let sSorted = s.split("").sort().join("")
-let tSorted = t.split("").sort().join("")
+let sToLower = s.toLowerCase()
+let tToLower = t.toLowerCase()
+
+let sSorted = sToLower.split("").sort().join("")
+let tSorted = tToLower.split("").sort().join("")
 //split -> [ 'l', 'i', 's', 't', 'e', 'n' ]
 //sort -> [ 'e', 'i', 'l', 'n', 's', 't' ]
 //join -> eilnst
